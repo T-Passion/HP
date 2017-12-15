4c3ec50dcd2e32b409265b17d428accb27124cd2
iwenchao <iwenchaos@gmail.com>
2017-12-15 15:34:57 +0800

    增加编码规范

--- app/src/main/java/com/passion/hp/module/home/HomeActivity.java
+++ app/src/main/java/com/passion/hp/module/home/HomeActivity.java
@@ -2,21 +2,31 @@ package com.passion.hp.module.home;
 
 
 import android.view.View;
+import android.widget.FrameLayout;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.passion.hp.R;
 import com.passion.libbase.AbstractBaseActivity;
 import com.passion.libbase.constants.RouterPath;
 import com.passion.libbase.imp.LayoutId;
+import com.passion.widget.main.NavigationTabBar;
+
+import butterknife.BindView;
 
 @LayoutId(R.layout.activity_home_layout)
 @Route(path = RouterPath.HOME_ACTIVITY)
 public class HomeActivity extends AbstractBaseActivity {
 
 
+    @BindView(R.id.home_content_container)
+    FrameLayout mContentContainer;
+    @BindView(R.id.home_tab_container)
+    NavigationTabBar mTabContainer;
 
     @Override
     protected void initVars(View view) {
+//        setTitleBar(null,getDrawable());
+
 
     }
 
@@ -25,4 +35,7 @@ public class HomeActivity extends AbstractBaseActivity {
 
     }
 
+    private void initTabBars(){
+
+    }
 }
