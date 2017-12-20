cff2a87782e3deb10a2000bd83fd26daa3505d68
iwenchao <iwenchaos@gmail.com>
2017-12-20 16:36:18 +0800

    计划

---
+++ lib.net/src/main/java/com/passion/libnet/api/INetService.java
@@ -0,0 +1,12 @@
+package com.passion.libnet.api;
+
+/**
+ * Created by chaos
+ * on 2017/12/20. 10:09
+ * 文件描述：定义通用的网络功能接口
+ */
+
+public interface INetService {
+
+
+}
--- sysbuild/doc/ToDoList.md
+++ sysbuild/doc/ToDoList.md
@@ -1,1 +1,3 @@
 1. 网络层
+2. 异常捕获，上传服务端
+3. 添加tinker布丁 [链接](https://mp.weixin.qq.com/s/rv7cUIOoLx60QZisAG0yyg)
