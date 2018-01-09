67c54d63b8a8b2d97bb006550a07277d1a806d9e
iwenchao <iwenchaos@gmail.com>
2018-01-09 14:21:05 +0800

    增加阿里短视频的sdk:调整

--- app/src/main/java/com/passion/hp/module/qupai/QuPaiActivity.java
+++ app/src/main/java/com/passion/hp/module/qupai/QuPaiActivity.java
@@ -4,10 +4,6 @@ import android.view.View;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.aliyun.demo.recorder.AliyunVideoRecorder;
-import com.aliyun.struct.common.VideoQuality;
-import com.aliyun.struct.recorder.CameraType;
-import com.aliyun.struct.recorder.FlashType;
-import com.aliyun.struct.snap.AliyunSnapVideoParam;
 import com.passion.hp.R;
 import com.passion.libbase.AbstractBaseActivity;
 import com.passion.libbase.constants.RouterPath;
@@ -26,25 +22,25 @@ public class QuPaiActivity extends AbstractBaseActivity {
 
     @Override
     protected void initVars(View view) {
-        AliyunSnapVideoParam recordParam = new AliyunSnapVideoParam.Builder()
-                //设置录制分辨率，目前支持360p，480p，540p，720p
-                .setResulutionMode(AliyunVideoRecorder.re)
-                //设置视频比例，目前支持1:1,3:4,9:16
-                .setRatioMode(AliyunVideoRecorder.RATIO_MODE_9_16)
-                .setRecordMode(RecorderDemo.RECORD_MODE_AUTO) //设置录制模式，目前支持按录，点录和混合模式
-                .setFilterList(eff_dirs) //设置滤镜地址列表,具体滤镜接口接收的是一个滤镜数组
-                .setBeautyLevel(80) //设置美颜度
-                .setBeautyStatus(true) //设置美颜开关
-                .setCameraType(CameraType.FRONT) //设置前后置摄像头
-                .setFlashType(FlashType.ON) // 设置闪光灯模式
-                .setNeedClip(true) //设置是否需要支持片段录制
-                .setMaxDuration(max) //设置最大录制时长 单位毫秒
-                .setMinDuration(min) //设置最小录制时长 单位毫秒
-                .setVideQuality(VideoQuality.EPD) //设置视频质量
-                .setGop(30) //设置关键帧间隔
-                .setVideoBitrate(2000) //设置视频码率，如果不设置则使用视频质量videoQulity参数计算出码率
-                .setSortMode(AliyunSnapVideoParam.SORT_MODE_VIDEO)//设置导入相册过滤选择视频
-                .build();
+//        AliyunSnapVideoParam recordParam = new AliyunSnapVideoParam.Builder()
+//                //设置录制分辨率，目前支持360p，480p，540p，720p
+//                .setResulutionMode(AliyunVideoRecorder.re)
+//                //设置视频比例，目前支持1:1,3:4,9:16
+//                .setRatioMode(AliyunVideoRecorder.RATIO_MODE_9_16)
+//                .setRecordMode(RecorderDemo.RECORD_MODE_AUTO) //设置录制模式，目前支持按录，点录和混合模式
+//                .setFilterList(eff_dirs) //设置滤镜地址列表,具体滤镜接口接收的是一个滤镜数组
+//                .setBeautyLevel(80) //设置美颜度
+//                .setBeautyStatus(true) //设置美颜开关
+//                .setCameraType(CameraType.FRONT) //设置前后置摄像头
+//                .setFlashType(FlashType.ON) // 设置闪光灯模式
+//                .setNeedClip(true) //设置是否需要支持片段录制
+//                .setMaxDuration(max) //设置最大录制时长 单位毫秒
+//                .setMinDuration(min) //设置最小录制时长 单位毫秒
+//                .setVideQuality(VideoQuality.EPD) //设置视频质量
+//                .setGop(30) //设置关键帧间隔
+//                .setVideoBitrate(2000) //设置视频码率，如果不设置则使用视频质量videoQulity参数计算出码率
+//                .setSortMode(AliyunSnapVideoParam.SORT_MODE_VIDEO)//设置导入相册过滤选择视频
+//                .build();
     }
 
     @Override
@@ -53,6 +49,5 @@ public class QuPaiActivity extends AbstractBaseActivity {
     }
 
     public void goRecordClick(View view) {
-        AliyunVideoRecorder.startRecordForResult(this, REQUEST_RECORD, recordParam);
     }
 }
